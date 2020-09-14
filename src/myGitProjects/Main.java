5404f727bb6799e09e6233031743076265fa2e5a
tzielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2020-09-14 07:08:33 +0200

    Check account existence, collecting data from file, some other corrections

--- src/myGitProjects/Main.java
+++ src/myGitProjects/Main.java
@@ -1,8 +1,6 @@
 package myGitProjects;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
@@ -10,50 +8,22 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner initialQuestions = new Scanner(System.in);
         System.out.println("Welcome to Tee Bank!");
-        String answer, fName, lName, log, pass;
+
+        String answer;
 
         do {
             System.out.print("Have you already got an account? (y/n): ");
             answer = initialQuestions.nextLine();
         } while (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n"));
 
-            if (answer.equalsIgnoreCase("n")) {
-                System.out.println("If you want to join us, enter some details about you and your future account, " +
-                        "please: ");
-                System.out.print("What is your name?: ");
-                fName = initialQuestions.nextLine();
-                System.out.print("What is your last name?: ");
-                lName = initialQuestions.nextLine();
-                System.out.print("Choose your login: ");
-                log = initialQuestions.nextLine();
-                System.out.print("Choose your password: ");
-                pass = initialQuestions.nextLine();
+            if (answer.equalsIgnoreCase("n")) account.setYourNewAccount();
 
-                account newAccount = new account(fName, lName,
-                        log, pass, 100);
+            else if (answer.equalsIgnoreCase("y")) {
+                account.checkIfAccountExistAndCollectData();
 
-
-                System.out.println("Hello " + newAccount.firstName + " " + newAccount.lastName + ", " +
-                        "you have established " + "your login to '" + newAccount.login +
-                        "' and your password to '" + newAccount.password + "'.");
-                System.out.println("And you have got a present from us - " + newAccount.balance +
-                        " PLN for the start!");
-                File newAccountFile = new File("accountOf" + fName + ".txt");
-                PrintWriter safeToFile = new PrintWriter("accountOf" + fName + ".txt");
-                safeToFile.println(newAccount.firstName);
-                safeToFile.println(newAccount.lastName);
-                safeToFile.println(newAccount.login);
-                safeToFile.println(newAccount.password);
-                safeToFile.println(newAccount.balance);
-                safeToFile.close();
-
-            } else if (answer.equalsIgnoreCase("y")) {
-                System.out.print("Login: ");
-
-                System.out.print("Password: ");
-
-            } else System.out.println("Invalid character!");
-
+            }
+            else System.out.println("Invalid character!");
+            }
 
     }
-}
+
--- src/myGitProjects/account.java
+++ src/myGitProjects/account.java
@@ -1,11 +1,16 @@
 package myGitProjects;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class account {
-    String firstName, lastName, login, password;
-
-    double balance;
-
-
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    private double balance;
 
     public account(String firstName, String lastName, String login, String password, double balance) {
         this.firstName = firstName;
@@ -15,13 +20,8 @@ public class account {
         this.balance = balance;
     }
 
-    public double getBalance() {
-        return balance;
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getFirstName(){return firstName; }
 
     public String getLastName() {
         return lastName;
@@ -34,4 +34,84 @@ public class account {
     public String getPassword() {
         return password;
     }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public static void setYourNewAccount() throws FileNotFoundException {
+        Scanner initialQuestions = new Scanner(System.in);
+        System.out.println("If you want to join us, enter some details about you and your future account, " +
+                "please: ");
+        System.out.print("What is your name?: ");
+        String firstName = initialQuestions.nextLine();
+        System.out.print("What is your last name?: ");
+        String lastName = initialQuestions.nextLine();
+        System.out.print("Choose your login: ");
+        String login = initialQuestions.nextLine();
+        System.out.print("Choose your password: ");
+        String password = initialQuestions.nextLine();
+
+        double balance = 100;
+
+        account newAccount = new account(firstName, lastName,
+                login, password, balance);
+
+
+        System.out.println("Hello " + newAccount.getFirstName() + " " + newAccount.getLastName() + ", " +
+                "you have established " + "your login to '" + newAccount.getLogin() +
+                "' and your password to '" + newAccount.getPassword() + "'.");
+        System.out.println("And you have got a present from us - " + balance +
+                " PLN for the start!");
+        PrintWriter safeToFile = new PrintWriter("accountOf_" +
+                newAccount.getLogin().substring(newAccount.getLogin().length()-3, newAccount.getLogin().length())
+                + newAccount.getPassword().substring(newAccount.getPassword().length()-3, newAccount.getPassword().length()) + ".txt");
+        safeToFile.println(newAccount.getFirstName());
+        safeToFile.println(newAccount.getLastName());
+        safeToFile.println(newAccount.getLogin());
+        safeToFile.println(newAccount.getPassword());
+        safeToFile.println(newAccount.getBalance());
+        safeToFile.close();
+    }
+
+    public static void checkIfAccountExistAndCollectData() throws FileNotFoundException {
+        Scanner checkLoginPassword = new Scanner(System.in);
+        System.out.print("Login: ");
+        String login = checkLoginPassword.nextLine();
+
+        System.out.print("Password: ");
+        String password = checkLoginPassword.nextLine();
+
+        File checkFile = new File("accountOf_" + login.substring(login.length()-3, login.length())
+                + password.substring(password.length()-3, password.length()) + ".txt");
+        if (checkFile.isFile()) {
+            Scanner readFile = new Scanner(checkFile);
+            String firstName = readFile.nextLine();
+            String lastName = readFile.nextLine();
+            login = readFile.nextLine();
+            password = readFile.nextLine();
+            String stringBalance = readFile.nextLine();
+
+            double balance = Double.parseDouble(stringBalance);
+
+            account existingAccount = new account(firstName, lastName, login, password, balance);
+            System.out.println("Welcome back " + existingAccount.getFirstName() + " " +
+                    existingAccount.getLastName() + "! Your balance is now: " +
+                    existingAccount.getBalance() + ".");
+            }
+        else {
+            System.out.println("Account does not exist!");
+            /*do {
+                System.out.print("Would you like us to set an account for you? (y/n): ");
+                answer = initialQuestions.nextLine();
+            }
+            while (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n"));
+
+            if (answer.equalsIgnoreCase("y")) account.setYourNewAccount();
+            else System.exit(0);*/
+        }
+    }
 }
+
+
+
